6db89cac77a603d264c13a6af3cd6ce894393ba3
belmu <belmuledozo@gmail.com>
2025-12-08 23:21:56 +0100

    Vendoring Slang binaries

---
+++ cmake/slang.cmake
@@ -0,0 +1,121 @@
+##########################################################
+#                      INSTALLATION                      #
+##########################################################
+
+find_package(slang CONFIG QUIET)
+
+if (NOT slang_FOUND)
+    message(STATUS "No installed Slang version found: downloading prebuilt binaries for this system...")
+
+    set(SLANG_RELEASE_TAG "v2025.23.2")
+    set(SLANG_RELEASE_URL "https://github.com/shader-slang/slang/releases/download/${SLANG_RELEASE_TAG}")
+
+    # Fetch binaries depending on the operating system
+    if(WIN32 AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(SLANG_BINARIES "slang-${SLANG_VERSION}-windows-x86_64.zip")
+    elseif(APPLE AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(SLANG_BINARIES "slang-${SLANG_VERSION}-macos-x86_64.zip")
+    elseif(APPLE AND CMAKE_SIZEOF_VOID_P EQUAL 8 AND CMAKE_SYSTEM_PROCESSOR MATCHES "arm")
+        set(SLANG_BINARIES "slang-${SLANG_VERSION}-macos-aarch64.zip")
+    elseif(UNIX AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+        set(SLANG_BINARIES "slang-${SLANG_VERSION}-linux-x86_64.tar.xz")
+    else()
+        message(
+            FATAL_ERROR "No prebuilt Slang asset configured for platform ${CMAKE_SYSTEM_NAME} / ${CMAKE_SYSTEM_PROCESSOR}"
+        )
+    endif()
+
+    # Set up download/install paths
+    set(SLANG_BINARIES_URL "${SLANG_RELEASE_URL}/${SLANG_BINARIES}")
+    set(SLANG_INSTALL_DIR "${CMAKE_BINARY_DIR}/thirdparty/slang")
+
+    # If compiler binary doesn't exist yet
+    if(NOT EXISTS "${SLANG_INSTALL_DIR}/bin/slangc" AND NOT EXISTS "${SLANG_INSTALL_DIR}/bin/slangc.exe")
+        # Download the binaries
+        file(DOWNLOAD "${SLANG_URL}" "${CMAKE_BINARY_DIR}/${SLANG_BINARIES}" SHOW_PROGRESS)
+
+        # Unpack (support both .zip and .tar.xz)
+        if("${SLANG_BINARIES}" MATCHES "\\.zip$")
+            execute_process(
+                COMMAND ${CMAKE_COMMAND} -E tar xzf "${CMAKE_BINARY_DIR}/${SLANG_BINARIES}"
+                WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+            )
+        else()
+            execute_process(
+                COMMAND ${CMAKE_COMMAND} -E tar xJf "${CMAKE_BINARY_DIR}/${SLANG_BINARIES}"
+                WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+            )
+        endif()
+
+        # Move extracted binaries to the install directory
+        file(GLOB CHILDREN RELATIVE "${CMAKE_BINARY_DIR}" "${CMAKE_BINARY_DIR}/*slang*")
+        file(MAKE_DIRECTORY "${SLANG_INSTALL_DIR}")
+
+        file(RENAME "${CMAKE_BINARY_DIR}/${CHILDREN}" "${SLANG_INSTALL_DIR}")
+    endif()
+
+    list(APPEND CMAKE_PREFIX_PATH "${SLANG_INSTALL_DIR}")
+
+    find_package(slang CONFIG REQUIRED)
+endif()
+
+target_link_libraries(NobleEngine PRIVATE slang::slang)
+
+##########################################################
+#                  SHADERS COMPILATION                   #
+##########################################################
+
+set(SHADERS_DIR_RELATIVE resources/shaders)
+set(SHADERS_DIR ${CMAKE_SOURCE_DIR}/${SHADERS_DIR_RELATIVE})
+
+function(add_slang_shader_target TARGET)
+    cmake_parse_arguments(SHADERS "" "" "SOURCES;ENTRIES" ${ARGN})
+
+    set(SHADERS_SPV_DIR_RELATIVE ${SHADERS_DIR_RELATIVE}/spv)
+    set(SHADERS_SPV_DIR ${SHADERS_DIR}/spv)
+    file(MAKE_DIRECTORY ${SHADERS_SPV_DIR})
+
+    set(SPV_OUTPUTS "")
+
+    foreach(SOURCE ${SHADERS_SOURCES})
+        cmake_path(GET SOURCE STEM SOURCE_NAME)
+
+        foreach(ENTRY_PAIR ${SHADERS_ENTRIES})
+            string(REPLACE "=" ";" ENTRY_SPLIT "${ENTRY_PAIR}")
+            list(GET ENTRY_SPLIT 0 ENTRY_POINT)
+            list(GET ENTRY_SPLIT 1 STAGE_NAME )
+
+            string(SUBSTRING "${STAGE_NAME}" 0 4 STAGE_NAME_SHORT)
+
+            set(SPV_FILE_NAME ${SOURCE_NAME}.${STAGE_NAME_SHORT}.spv)
+            set(SPV_FILE ${SHADERS_SPV_DIR}/${SPV_FILE_NAME})
+            list(APPEND SPV_OUTPUTS ${SPV_FILE})
+
+            # Run Slangc command
+            add_custom_command(
+                OUTPUT ${SPV_FILE}
+                COMMAND "${SLANGC_EXECUTABLE}" ${SOURCE}
+                -target spirv
+                -entry ${ENTRY_POINT}
+                -stage ${STAGE_NAME}
+                -profile spirv_1_4
+                -emit-spirv-directly
+                -fvk-use-entrypoint-name
+                -o ${SPV_FILE}
+                DEPENDS ${SOURCE}
+                COMMENT "Building SPIR-V object ${SHADERS_SPV_DIR_RELATIVE}/${SPV_FILE_NAME}"
+                VERBATIM
+            )
+        endforeach()
+    endforeach()
+
+    add_custom_target(${TARGET} DEPENDS ${SPV_OUTPUTS})
+endfunction()
+
+file(GLOB_RECURSE SHADERS_SOURCES ${SHADERS_DIR}/src/*.slang)
+
+add_slang_shader_target(
+    SlangShaders
+    SOURCES ${SHADERS_SOURCES}
+    ENTRIES "fragMain=fragment" "vertMain=vertex"
+)
